3c315251b205ab68183acbfe740db0fae49c843b
Nace Gorenc <ng0632@student.uni-lj.si>
2020-03-21 17:25:15 +0100

    home fragment xml done

---
+++ app/src/main/java/si/uni_lj/fri/pbd/miniapp1/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package si.uni_lj.fri.pbd.miniapp1.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
