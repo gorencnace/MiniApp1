bc32c32abed120fbd058e83b2a93ed5566e8a422
Nace Gorenc <ng0632@student.uni-lj.si>
2020-03-21 19:31:00 +0100

    little changes in fragment code, not essential

--- app/src/main/java/si/uni_lj/fri/pbd/miniapp1/ui/contacts/ContactsFragment.java
+++ app/src/main/java/si/uni_lj/fri/pbd/miniapp1/ui/contacts/ContactsFragment.java
@@ -2,8 +2,6 @@ package si.uni_lj.fri.pbd.miniapp1.ui.contacts;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -13,9 +11,9 @@ import android.view.ViewGroup;
 import si.uni_lj.fri.pbd.miniapp1.R;
 
 public class ContactsFragment extends Fragment {
-    @Nullable
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_contacts, container, false);
     }
 }
